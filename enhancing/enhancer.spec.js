0f7ecb6e7da626e0c59c9f419524297eee136d5b
Gerardo Rivera <gerardo.rivera3901@gmail.com>
2020-12-16 14:13:40 -0800

    Completed MVP

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,24 @@
 const enhancer = require('./enhancer.js');
-// test away!
+
+describe('sanity check', () => {
+  it('works', () => {
+    expect(2 + 2).toBe(4);
+  });
+});
+
+const testItem = {name: 'blade', durability: 12, enhancement: 4};
+
+describe('Enhancer Functions', () => {
+  it('can repair item', () => {
+    const repairedItem = {name: 'blade', durability: 100, enhancement: 4};
+    expect(enhancer.repair(testItem)).toEqual(repairedItem);
+  });
+  it('can successfully enhance', () => {
+    const enhancedItem = {name: 'blade', durability: 100, enhancement: 5};
+    expect(enhancer.success(testItem)).toEqual(enhancedItem);
+  });
+  it('can fail enhancements', () => {
+    const failedItem = {name: 'blade', durability: 95, enhancement: 5};
+    expect(enhancer.fail(testItem)).toEqual(failedItem);
+  });
+});
